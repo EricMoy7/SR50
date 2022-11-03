ea5f6f7c863179c8f85caf3550f83322b5bf0c65
Eric <ericzmoy@gmail.com>
2022-11-02 21:01:15 -0500

    All inputs

--- src/components/FinanceForm.js
+++ src/components/FinanceForm.js
@@ -70,6 +70,69 @@ export default function FinanceForm() {
 					value={financialData.housingBurn}
 					onChange={(e) => handleChange(e)}
 				/>
+
+				<TextField
+					type="number"
+					id="transportationBurn"
+					label="Transportation Burn"
+					name="transportationBurn"
+					value={financialData.transportationBurn}
+					onChange={(e) => handleChange(e)}
+				/>
+
+				<TextField
+					type="number"
+					id="foodBurn"
+					label="Food Burn"
+					name="foodBurn"
+					value={financialData.foodBurn}
+					onChange={(e) => handleChange(e)}
+				/>
+
+				<TextField
+					type="number"
+					id="insuranceBurn"
+					label="Insurance Burn"
+					name="insuranceBurn"
+					value={financialData.insuranceBurn}
+					onChange={(e) => handleChange(e)}
+				/>
+
+				<TextField
+					type="number"
+					id="badDebtBalance"
+					label="Bad Debt Balance"
+					name="badDebtBalance"
+					value={financialData.badDebtBalance}
+					onChange={(e) => handleChange(e)}
+				/>
+
+				<TextField
+					type="number"
+					id="passiveCashFlow"
+					label="Passive Cash Flow"
+					name="passiveCashFlow"
+					value={financialData.passiveCashFlow}
+					onChange={(e) => handleChange(e)}
+				/>
+
+				<TextField
+					type="number"
+					id="desiredLifestyleCost"
+					label="Desired Lifestyle Cost"
+					name="desiredLifestyleCost"
+					value={financialData.desiredLifestyleCost}
+					onChange={(e) => handleChange(e)}
+				/>
+
+				<TextField
+					type="number"
+					id="bigLifeGoalsBurn"
+					label="Big Life Goals Burn"
+					name="bigLifeGoalsBurn"
+					value={financialData.bigLifeGoalsBurn}
+					onChange={(e) => handleChange(e)}
+				/>
 			</div>
 		</Box>
 	);
